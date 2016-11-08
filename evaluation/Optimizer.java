40f1ddec39a7267db190c015c60be1b86fcf530d
yantangucsb <tangyan0627@gmail.com>
2016-11-08 13:49:05 -0800

    continue

--- evaluation/Optimizer.java
+++ evaluation/Optimizer.java
@@ -12,33 +12,62 @@ import org.apache.commons.lang3.SerializationUtils;
 import model.incident.Incident;
 import model.incidentree.*;
 import model.incidentree.IncidentTreeNode.NodeType;
+import model.log.Activity;
 
 public class Optimizer {
+	static double filterRate = 10.0;
+	static double epsilon = 1e-3;
+	static int limitedSearchSpace = Integer.MAX_VALUE;
+	static Set<String> visited;
 
-	public static void generateOptimalTree(Incident incident) {
+	public static void generateOptimalTree(Incident incident, int model) {
 		incident.optiTree = incident.tree;
 		IncidentTree curTree = SerializationUtils.clone(incident.tree);
 		Queue<IncidentTree> frontier = new LinkedList<IncidentTree>();
 		frontier.add(curTree);
-		Set<String> visited = new HashSet<String>();
+		visited = new HashSet<String>();
+		double optiCost = estimateCost(incident.optiTree, model);
+		int count = 0;
 		while(!frontier.isEmpty()){
 			IncidentTree front = frontier.remove();
-			if(estimateCost(front) < estimateCost(incident.optiTree)){
+			count++;
+			if(count == limitedSearchSpace)
+				break;
+			double curCost = estimateCost(front, model);
+			if(curCost < optiCost){
+				optiCost = curCost;
 				incident.optiTree = front;
+				if(optiCost < epsilon)
+					break;
 			}
-			System.out.println(front.toString());
+//			System.out.println("[Debug] Frontier: " + front.toString() + "\nCost: " + curCost);
+			
 			visited.add(front.toString());
 			List<IncidentTree> li = new ArrayList<IncidentTree>();
 			generate(front, front.root, li, "");
 			for(IncidentTree it: li){
-				if(!visited.contains(it.toString())){
-					frontier.add(it);
-					visited.add(it.toString());
-				}
+				frontier.add(it);
+				visited.add(it.toString());
 			}
+//			System.out.println("Runtime: " + testRuntime(front));
 		}
+//		System.out.println(incident.optiTree.toString());
 	}
 	
+	private static long testRuntime(IncidentTree front) {
+		long t1 = System.currentTimeMillis();
+		Thread t = new Thread(front.root);
+		t.start();
+		try {
+			t.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		long t2 = System.currentTimeMillis();
+		return t2 - t1;
+	}
+
 	private static void generate(IncidentTree curTree, IncidentTreeNode cur,
 			List<IncidentTree> li, String accessCode) {
 		if(cur == null)
@@ -47,7 +76,11 @@ public class Optimizer {
 			if(oprule.checkQualified(cur)){
 				IncidentTree tmp = SerializationUtils.clone(curTree);
 				oprule.perform(tmp, cur, accessCode);
-				li.add(tmp);
+//				System.out.println("[Debug] Perform: " + tmp.toString());
+				if(!visited.contains(tmp.toString())){
+					li.add(tmp);
+				}
+				
 			}
 		}
 		generate(curTree, cur.left, li, accessCode+"0");
@@ -55,25 +88,68 @@ public class Optimizer {
 	}
 
 
-	public static double estimateCost(IncidentTree it){
+	public static double estimateCost(IncidentTree it, int model){
 		if(it == null)
 			return 0;
-		return estimate(it.root);
+		return estimate(it.root, model).cost;
 	}
 
-	private static double estimate(IncidentTreeNode root) {
+	//normalized cost with log size -- 11/4/2016
+	private static CostModel estimate(IncidentTreeNode root, int model) {
+		long logSize = QueryEngine.queryEngine.log.size();
+		CostModel curModel = null;
+		
 		if(root == null)
-			return 0;
-		if(root.getType() == NodeType.ACTI){
-			if(QueryEngine.queryEngine.log.actiStat.containsKey(root.name))
-				return QueryEngine.queryEngine.log.actiStat.get(root.name).count;
-			System.err.println("Current log doesn't have the activity: " + root.name);
-			return 0;
+			return null;
+		else if(root.getType() == NodeType.ACTI){
+//			System.err.println("[Debug] estimate: Node name --" + root.name);
+			if(QueryEngine.queryEngine.log.actiStat.containsKey(root.name)){
+				curModel = new CostModel(QueryEngine.queryEngine.log.actiStat.get(root.name));
+				curModel.cost = 1;
+			}else
+//			System.err.println("Current log doesn't have the activity: " + root.name);
+				curModel = new CostModel(root.name);
 		}else if(root.getType() == NodeType.COND){
-			return estimate(root.left)/3.0;
+			CostModel costLeft = estimate(root.left, model);
+			if(costLeft == null || costLeft.count == 0){
+				curModel = costLeft;
+			}else{
+				//condition node only reduce the count
+				costLeft.count /= filterRate;
+				costLeft.cost = costLeft.cost + costLeft.count/logSize;
+				curModel = costLeft;
+			}
 		}else{
-			return QueryEngine.queryEngine.operators.get(root.name).getCost1(estimate(root.left), estimate(root.right));
+			CostModel costLeft = estimate(root.left, model);
+			CostModel costRight = estimate(root.right, model);
+			double curCost = Math.max(costLeft.cost, costRight.cost);
+			int len = costLeft.len + costRight.len;
+			double normLeftSize = (double)costLeft.count/logSize;
+			double normRightSize = (double)costRight.count/logSize;
+			switch(root.name){
+			//binary search: normLeftSize*(Math.log(normRightSize)/Math.log(2))
+			//linear scan: normLeftSize*normRightSize
+			case ".": curCost += normLeftSize == 0 || normRightSize == 0? 0: normLeftSize*costRight.count; break;
+			case ":": curCost += normLeftSize*costRight.count; break;
+			case "|": curCost += normLeftSize + normLeftSize; 
+				      len = Math.max(costLeft.len, costRight.len); break;
+			case "+": curCost += normLeftSize * costRight.count * len; break;
+			default: break;
+			}
+			
+			if(model == 1){
+				long curSize = QueryEngine.queryEngine.operators.get(root.name).getResultSize1(costLeft.count, costRight.count);
+				curModel = new CostModel(costLeft.name);
+				curModel.count = curSize;
+			}else
+				curModel = QueryEngine.queryEngine.operators.get(root.name).getResultSize2(costLeft, costRight);
+			
+			curModel.cost = curCost;
+			curModel.len = len;
+			
 		}
+//		System.out.println("[Node " + root.name + " -- Estimated Cost:" + curModel.cost + " Count: " + curModel.count);
+		return curModel;
 	}
 
 }
--- test/TestOptimizer.java
+++ test/TestOptimizer.java
@@ -12,13 +12,16 @@ import evaluation.Optimizer;
 import evaluation.QueryEngine;
 
 public class TestOptimizer extends Optimizer {
-	Log log = new Log("data/tranlog.txt");
+	Log log = new Log("data/output_07.txt");
+//	Log log = new Log("data/tranlog.txt");
 
 	@Test
 	public void testCostModel() {
 		QueryEngine.queryEngine.log = log;
-		Incident incident = new Incident("start.GetRefer");
-		System.out.println(Optimizer.estimateCost(incident.tree));
+		System.out.println(QueryEngine.queryEngine.log.statInfo());
+		Incident incident = new Incident("start.GetRefer.SeeDoctor");
+		System.out.println(Optimizer.estimateCost(incident.tree, 1));
+		
 	}
 	
 	@Test
@@ -32,40 +35,66 @@ public class TestOptimizer extends Optimizer {
 	@Test
 	public void testAssociativeRule() {
 		QueryEngine.queryEngine.log = log;
-		Incident incident = new Incident("start.(GetRefer:GetReimburse)");
-		Optimizer.generateOptimalTree(incident);
+		Incident incident = new Incident("start.GetRefer:GetReimburse");
+		Optimizer.generateOptimalTree(incident, 1);
 		System.out.println("---------------");
-		Incident incident1 = new Incident("start.(GetRefer:(UpdateRefer.GetReimburse))");
-		Optimizer.generateOptimalTree(incident1);
+		Incident incident1 = new Incident("start:GetRefer.UpdateRefer.GetReimburse");
+		Optimizer.generateOptimalTree(incident1, 1);
 	}
 	
 	@Test
 	public void testCommutativeRule() {
 		QueryEngine.queryEngine.log = log;
 		Incident incident = new Incident("start+GetRefer");
-		Optimizer.generateOptimalTree(incident);
+		Optimizer.generateOptimalTree(incident, 1);
 		System.out.println("---------------");
 		Incident incident1 = new Incident("start.(GetRefer+UpdateRefer)");
-		Optimizer.generateOptimalTree(incident1);
+		Optimizer.generateOptimalTree(incident1, 1);
 	}
 	
 	@Test
 	public void testConditionRule() {
 		QueryEngine.queryEngine.log = log;
 		Incident incident = new Incident("[x=1](GetRefer.GetReimburse)[y=1]");
-		Optimizer.generateOptimalTree(incident);
+		Optimizer.generateOptimalTree(incident, 1);
 		
 		System.out.println("---------------");
 		Incident incident1 = new Incident("start.([x=1](GetRefer.UpdateRefer)[y=1])");
-		Optimizer.generateOptimalTree(incident1);
+		Optimizer.generateOptimalTree(incident1, 1);
 	}
 	
 	@Test
 	public void testDistributiveRule() {
 		QueryEngine.queryEngine.log = log;
-		Incident incident = new Incident("start.(GetRefer|GetReimburse)");
-		Optimizer.generateOptimalTree(incident);
-		
+		Incident incident = new Incident("a.b.(c|d)");
+		Optimizer.generateOptimalTree(incident, 1);
+		System.out.println(incident.optiTree.toString());
+	}
+	
+	@Test
+	public void testOptimizer(){
+		QueryEngine.queryEngine.log = log;
+		String[] qs = {"CULTURE+RADIOLOG.INJMECH+OPRM:TREATMEN",
+				"VITALS|INJMECH+QAISSUE|TTDETLS|PROTECT",
+				"CULTURE+MTOS:EMERG:MAINDATA.QAISSUE",
+				"TRANSPRT|NARRATIV:PERHIST+BURNS:ICU",
+				"TRANSFER+LAB:GENMECH|NARRATIV:RADIOLOG",
+				"TRANSPRT|STEP.CULTURE:INJDETS.STEP",
+				"EMERG+TRA+LAB+HEMO:GENMECH",
+				"TRANSPRT.CULTURE.MAINDATA+INJDETS.STEP",
+				"GENMECH+MTOS:CONSULT:CULTURE+WARD",
+				"HOSPREV|ORGANS:MAINDATA+TRA+TRANSFER"};
+/*		for(String q: qs){
+			System.out.println(q);
+			Incident incident = new Incident(q);
+			System.out.println(incident.tree.toString());
+			Optimizer.generateOptimalTree(incident, 2);
+			System.out.println(incident.optiTree.toString());
+		}*/
+		Incident incident = new Incident(qs[9]);
+		System.out.println(incident.tree.toString());
+		Optimizer.generateOptimalTree(incident, 2);
+		System.out.println(incident.optiTree.toString());
 	}
 
 }
